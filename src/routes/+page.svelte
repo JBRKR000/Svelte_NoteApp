acbd0b9870e4c49f9529ce163f22d8e0a5dba332
JBRKR000 <kozimorjakub@gmail.com>
2025-06-17 18:53:26 +0200

    refactor +page.svelte to conditionally render components based on animation state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,28 @@
 <script>
+    import { fade, fly } from "svelte/transition";
     import Navbar from "$lib/components/Navbar.svelte";
     import Footer from "$lib/components/Footer.svelte";
     import PresentationSection from "$lib/components/PresentationSection.svelte";
-	import HowItWorks from "$lib/components/HowItWorks.svelte";
+    import HowItWorks from "$lib/components/HowItWorks.svelte";
+    import { onMount } from "svelte";
+    let show = false;
     let animationFinished = false;
+
+    onMount(() => {
+        setTimeout(() => show = true, 10);
+    });
 </script>
 
-<head>
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
-</head>
+<div class="fixed inset-0 -z-10 bg-gradient-to-t from-indigo-950 to-black"></div>
 
-
-<div class="min-h-screen font-manrope bg-gradient-to-t from-indigo-950 to-black">
-    <Navbar />
-    <PresentationSection bind:animationFinished />
-    <HowItWorks {animationFinished}/>
-    <Footer />
-</div>
+{#if show}
+    <div
+        class="min-h-screen font-manrope relative"
+        transition:fly={{ y: 40, opacity: 0, duration: 600, delay: 80 }}
+    >
+        <Navbar />
+        <PresentationSection bind:animationFinished />
+        <HowItWorks {animationFinished}/>
+        <Footer />
+    </div>
+{/if}
